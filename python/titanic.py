bab9bfb3d75cdc6b27a766e7e0daa4f4c79dd08d
ritterchuang <ritterchuang@simbo.com.tw>
2018-07-14 17:44:56 +0800

    first version.

---
+++ python/titanic.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 14 17:13:14 2018
+
+@author: User
+"""
+from sklearn import preprocessing 
+from sklearn.model_selection import GridSearchCV 
+from sklearn.ensemble import RandomForestClassifier 
+from sklearn.ensemble import RandomForestRegressor
+
+import warnings
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os as os
+
+train = pd.read_csv("../resources/train.csv")
+test = pd.read_csv("../resources/test.csv")
+submit = pd.read_csv("../resources/gender_submission.csv")
+
+
+
+
